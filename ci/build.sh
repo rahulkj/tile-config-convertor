055821dbfec6e3d9da9903851d82ecfa46c9829d
Rahul Jain <rahulkj@gmail.com>
2020-02-21 21:17:50 -0600

    adding ci to build the releases

---
+++ ci/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+go test
+
+if [[ -d releases ]]; then
+  rm -rf releases
+fi
+
+mkdir releases
+
+GOOS=linux GOARCH=amd64 go build -o releases/tile-config-convertor_linux_amd64 github.com/rahulkj/tile-config-convertor
+
+GOOS=darwin GOARCH=amd64 go build -o releases/tile-config-convertor-darwin_amd64 github.com/rahulkj/tile-config-convertor
+
+GOOS=windows GOARCH=386 go build -o releases/tile-config-convertor-windows_amd64.exe github.com/rahulkj/tile-config-convertor
